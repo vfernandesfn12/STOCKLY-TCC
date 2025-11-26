f8318d529d87a6a22b95db9af5bd8fb6805fa7c9
VITOR HUGO ZAMBORLINI MASIOL <v.filho@labcetec.net>
2025-11-26 20:04:44 -0300

    Alteração

--- client/src/components/BarraNavegacao/BarraNavegacao.jsx
+++ client/src/components/BarraNavegacao/BarraNavegacao.jsx
@@ -70,12 +70,6 @@ const BarraNavegacao = () => {
               Home
             </Nav.Link>
 
-            {/* Link para Relatórios */}
-            <Nav.Link as={NavLink} to="/relatorios">
-              <HiMiniDocumentText className="me-1" />
-              Relatórios
-            </Nav.Link>
-
             {/* ===== MENU PRODUTOS ===== */}
             <NavDropdown
               title={
@@ -129,30 +123,12 @@ const BarraNavegacao = () => {
                 Adicionar
               </NavDropdown.Item>
             </NavDropdown>
-
-            {/* ===== MENU PEDIDOS ===== */}
-            <NavDropdown
-              title={
-                <>
-                  <HiMiniClipboardDocumentList className="me-1" />
-                  Pedidos
-                </>
-              }
-              id="pedidos-dropdown"
-            >
-              <NavDropdown.Item as={NavLink} to="/pedidos">
-                Listar
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/pedidos/cadastrar">
-                Adicionar
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
 
           {/* ==== PERFIL DO USUÁRIO ==== */}
           <Nav className="d-flex align-items-center mx-auto">
             {/* Botão de modo escuro (DarkMode) */}
-            <div className="me-3" style={{padding: "15px"}}>
+            <div className="me-3" style={{ padding: "15px" }}>
               <DarkMode />
             </div>
 
